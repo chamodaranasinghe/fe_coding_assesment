937a7cd3be44f34ed6d1958af93d4b9e6211b50c
Chamoda Ranasinghe <chamodam@gmail.com>
2024-04-10 02:43:24 +0700

    added unit tests for processMovieDetails

--- packages/MovieDataStore/tests/helper.spec.ts
+++ packages/MovieDataStore/tests/helper.spec.ts
@@ -1,10 +1,16 @@
-import sample from "./sample-movie-search-result.json";
-import { processSearchResults } from "../lib/movies/helper";
+import sampleSearchResult from "./sample-movie-search-result.json";
+import sampleDetailsResult from "./sample-movie-details-result.json";
+import {
+  processMovieDetails,
+  processSearchResults,
+} from "../lib/movies/helper";
 import { MovieSearchResult } from "../lib/types/api-responses/movie-search-result";
+import { DetailsResult } from "../lib/types/api-responses/movie-details";
+import exp from "constants";
 
 describe("Helper", () => {
-  it("should process json response correctly", () => {
-    const response = sample as MovieSearchResult;
+  it("should process search result json response correctly", () => {
+    const response = sampleSearchResult as MovieSearchResult;
     expect(response.ok).toBe(true);
     expect(processSearchResults(response).length).toBe(
       response.description.length
@@ -19,6 +25,35 @@ describe("Helper", () => {
       "#IMG_POSTER":
         "https://m.media-amazon.com/images/M/MV5BOGJjODQxYWUtYjlkOC00NzJiLTgyZjUtY2Q3Y2FhZGVlODEwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
       "#IMDB_URL": "https://imdb.com/title/tt13210838",
+    });
+  });
+
+  it("should process details result json response correctly", () => {
+    const response = sampleDetailsResult as unknown as DetailsResult;
+    expect(processMovieDetails(response)).toEqual({
+      id: "tt5433138",
+      name: "F9",
+      description:
+        "Dom and the crew must take on an international terrorist who turns out to be Dom and Mia&apos;s estranged brother.",
+      featuredReviews: [
+        {
+          summary: "Enough. Red light. Stop",
+          text: "Just utter garbage. To say this movie was written and made by a 12 year old is an insult to a 12 year old. This franchise needs to end now.",
+        },
+      ],
+      genre: ["Action", "Crime", "Thriller"],
+      keywords: [
+        "fast and furious franchise",
+        "car",
+        "gunfight",
+        "shootout",
+        "gun battle",
+      ],
+      actors: ["Vin Diesel", "Michelle Rodriguez", "Jordana Brewster"],
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMzllNmFmMjUtNDViYS00MzZiLThiOGUtMTU5OWM0NGY4ZGMxXkEyXkFqcGdeQXVyNzI1NzMxNzM@._V1_.jpg",
+      rating: 5.2,
+      totalReviews: 2444,
     });
   });
 });
