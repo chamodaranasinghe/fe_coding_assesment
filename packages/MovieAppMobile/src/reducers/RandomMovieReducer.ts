3ec647668a5bdb00e5b0f21b507058e02f7d87d4
Chamoda Ranasinghe <chamodam@gmail.com>
2024-04-10 13:55:21 +0700

    updated random movies slice to handle loading, error

--- packages/MovieAppMobile/src/reducers/RandomMovieReducer.ts
+++ packages/MovieAppMobile/src/reducers/RandomMovieReducer.ts
@@ -2,10 +2,13 @@ import {MovieSummary} from '@assessment/datastore';
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 interface RandomMovieState {
+  loading: boolean;
   list: MovieSummary[];
+  error?: string;
 }
 
 let initialState: RandomMovieState = {
+  loading: false,
   list: [],
 };
 
@@ -16,8 +19,15 @@ const randomMovieSlice = createSlice({
     setRandomMovies: (state, action: PayloadAction<MovieSummary[]>) => {
       state.list = action.payload;
     },
+    setRandomMoviesLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
+    },
+    setRandomMoviesError: (state, action: PayloadAction<string>) => {
+      state.error = action.payload;
+    },
   },
 });
 
-export const {setRandomMovies} = randomMovieSlice.actions;
+export const {setRandomMovies, setRandomMoviesLoading, setRandomMoviesError} =
+  randomMovieSlice.actions;
 export default randomMovieSlice.reducer;
