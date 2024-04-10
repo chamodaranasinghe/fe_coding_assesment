20dc7edc7f4305c9f8c51bcf09e3f7106ba90bb1
Chamoda Ranasinghe <chamodam@gmail.com>
2024-04-10 13:06:23 +0700

    added the initial home page

---
+++ packages/MovieAppMobile/src/screens/HomePage.tsx
@@ -0,0 +1,17 @@
+/*
+    This is the main page of the application. It will have a search bar on top, a list of all random movies in the beginning
+    Onces user searches for a movie, it will show the search results. User can click on any movie to see the details of the movie.
+*/
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function HomePage() {
+  return (
+    <View>
+      <Text>HomePage</Text>
+    </View>
+  );
+}
+
+export default HomePage;
