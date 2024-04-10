e53d728d5d7031e549b2f6bb78c4d86f83debec7
Chamoda Ranasinghe <chamodam@gmail.com>
2024-04-10 14:26:29 +0700

    changed metro config to watch movie @assessment/datastore library

--- packages/MovieAppMobile/metro.config.js
+++ packages/MovieAppMobile/metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,8 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [path.join(__dirname, '../../packages')],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
