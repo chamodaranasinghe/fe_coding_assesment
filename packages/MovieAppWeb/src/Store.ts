5cf329b0f20143197d4657e25db468eaf27252e9
Chamoda Ranasinghe <chamodam@gmail.com>
2024-04-11 15:48:08 +0700

    Configured the store

--- packages/MovieAppWeb/src/Store.ts
+++ packages/MovieAppWeb/src/Store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import RandomMovieReducer from "./reducers/RandomMovieReducer";
+import SearchMovieReducer from "./reducers/SearchMovieReducer";
+import DetailedMovieReducer from "./reducers/DetailedMovieReducer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    RandomMovies: RandomMovieReducer,
+    SearchMovies: SearchMovieReducer,
+    MovieDetails: DetailedMovieReducer,
+  },
 });
-
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
